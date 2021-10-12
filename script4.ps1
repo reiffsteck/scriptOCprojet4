65ddb564a1908ff6aefa3349ee11d9d037d11a31
Laurent Reiffsteck <reiffsteck@gmail.com>
2021-10-12 22:42:46 +0200

    ligne de commande start-process ok test sur start-sleep

--- script4.ps1
+++ script4.ps1
@@ -1,5 +1,5 @@
 #creation de la ligne de commande de sauvevarde
-Start-Process -FilePath "$CurrentPath\robocopy.exe" -ArgumentList "c:\datapc c:\sav /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " -Wait
-
-#attente du script, ceci estun test
+Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\datapc c:\sav /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " -Wait
+#$currentpath pour indiquer le chemin 
+#attente du script, ceci est un test
 Start-Sleep 100
