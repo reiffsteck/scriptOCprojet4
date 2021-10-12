3873819918d0e5ca995ae0e5584d0760de4f6989
Laurent Reiffsteck <reiffsteck@gmail.com>
2021-10-12 22:44:00 +0200

    start-sleep 100 un peu trop long

--- script4.ps1
+++ script4.ps1
@@ -2,4 +2,4 @@
 Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\datapc c:\sav /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " -Wait
 #$currentpath pour indiquer le chemin 
 #attente du script, ceci est un test
-Start-Sleep 100
+Start-Sleep 1
