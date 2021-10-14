3832f1416028d671728b082f884a8c4b5e78a55c
Laurent Reiffsteck <reiffsteck@gmail.com>
2021-10-14 23:10:00 +0200

    chemin serveru introduit dans la commande

--- script4.ps1
+++ script4.ps1
@@ -7,7 +7,8 @@ param (
 $nompc=[System.Environment]::MachineName
 
 #creation de la ligne de commande de sauvevarde
-Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\datapc c:\sav /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " -Wait
+Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\datapc \\SRVACME\sav /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " -Wait
+#chemin des dossiers utilisateurs c:\utilisateurs
 #$currentpath pour indiquer le chemin variable $nompc
 #attente du script, ceci est un test
 Start-Sleep 1
