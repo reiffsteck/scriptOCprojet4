5651dca333344be6168815c9717c3e4e90d60cc8
Laurent Reiffsteck <reiffsteck@gmail.com>
2021-10-14 20:56:06 +0200

    recuperation du nom pc pour indiquer le chemin de la sauvegarde

--- script4.ps1
+++ script4.ps1
@@ -1,5 +1,13 @@
+param (
+        [string] $nompc
+
+      )
+
+#recuperation du nom pc pour l'adresse de sauvegarde
+$nompc=[System.Environment]::MachineName
+
 #creation de la ligne de commande de sauvevarde
 Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\datapc c:\sav /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " -Wait
-#$currentpath pour indiquer le chemin 
+#$currentpath pour indiquer le chemin variable $nompc
 #attente du script, ceci est un test
 Start-Sleep 1
