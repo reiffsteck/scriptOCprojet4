f3593e7021c94d3c320d7b523c37ce19715ef9e1
Laurent Reiffsteck <reiffsteck@gmail.com>
2021-10-16 18:42:46 +0200

    Foreach avec repertoire et startprocess

--- script4.ps1
+++ script4.ps1
@@ -1,14 +1,25 @@
 param (
-        [string] $nompc
+        [string] $nompc,
+        [string] $ListeRepertoire,
+        [string] $nomuser
 
       )
 
 #recuperation du nom pc pour l'adresse de sauvegarde
 $nompc=[System.Environment]::MachineName
 #lecture des répertoires sous c:\users 
-Get-ChildItem -Path C:\Users -Exclude Public -Name
+$ListeRepertoire = Get-ChildItem -Path C:\Users -Exclude Public 
+
+foreach ($repertoires in $ListeRepertoire)
+      {
+        $nomuser = $repertoires.Name
+        Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\datapc \\SRVACME\sav$\$nompc /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " 
+        #vrai ligne de commande
+        #Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\users\$nomuser\Documents \\SRVACME\sav$\$nompc /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " 
+      }
+
 #creation de la ligne de commande de sauvevarde
-Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\datapc \\SRVACME\sav$\$nompc /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " 
+
 #chemin des dossiers utilisateurs c:\utilisateurs
 #$currentpath pour indiquer le chemin variable $nompc
 #attente du script, ceci est un test
