1b61dfdfaa3c74be07c891dece4bea0c5f82cc75
Laurent Reiffsteck <reiffsteck@gmail.com>
2021-10-16 09:52:34 +0200

    juste la ligne de commande

--- script4.ps1
+++ script4.ps1
@@ -7,8 +7,8 @@ param (
 $nompc=[System.Environment]::MachineName
 
 #creation de la ligne de commande de sauvevarde
-Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\datapc \\SRVACME\sav$\$nompc /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " -Wait
+Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\datapc \\SRVACME\sav$\$nompc /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " 
 #chemin des dossiers utilisateurs c:\utilisateurs
 #$currentpath pour indiquer le chemin variable $nompc
 #attente du script, ceci est un test
-Start-Sleep 1
+#Start-Sleep 1
