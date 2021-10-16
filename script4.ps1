4844fd41506f898064bf3c11e481e37755a75b01
Laurent Reiffsteck <reiffsteck@gmail.com>
2021-10-16 18:03:56 +0200

    lecture répertoire

--- script4.ps1
+++ script4.ps1
@@ -5,7 +5,8 @@ param (
 
 #recuperation du nom pc pour l'adresse de sauvegarde
 $nompc=[System.Environment]::MachineName
-
+#lecture des répertoires sous c:\users 
+Get-ChildItem -Path C:\Users -Exclude Public -Name
 #creation de la ligne de commande de sauvevarde
 Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\datapc \\SRVACME\sav$\$nompc /copy:DAT /E /TS /FP /V /LOG+:c:\data\robo1.log /TEE " 
 #chemin des dossiers utilisateurs c:\utilisateurs
