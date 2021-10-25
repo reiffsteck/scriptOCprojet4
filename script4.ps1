4f884427d5223fc3bbf5037b1895fac455cc9de3
Laurent Reiffsteck <reiffsteck@gmail.com>
2021-10-25 21:11:57 +0200

    correction des chemins et repertoires cachés

--- script4.ps1
+++ script4.ps1
@@ -4,11 +4,11 @@ param (
         [string] $nomuser
 
       )
-
+Clear-host
 #recuperation du nom pc pour l'adresse de sauvegarde
 $nompc=[System.Environment]::MachineName
 #lecture des répertoires sous c:\users 
-$ListeRepertoire = Get-ChildItem -Path C:\Users -Exclude Public 
+$ListeRepertoire = Get-ChildItem -Path C:\Users -Exclude Public | Select Name
 
 #boucle pour chaque répertoire représentant un utilisateur
 # test de présence des répertoires utilisateurs et nom ordinateur
@@ -16,8 +16,15 @@ $ListeRepertoire = Get-ChildItem -Path C:\Users -Exclude Public
 foreach ($repertoires in $ListeRepertoire)
       {
         $nomuser = $repertoires.Name
-        if ((test-path "\\SRVACME\sav$\$nompc") -eq $true) 
+        Write-Host "coucou"
+        Write-Host $ListeRepertoire
+        Write-Host $repertoires
+        Write-Host $nomuser
+        Write-Host $nompc
+
+                if ((test-path "\\SRVACME\sav$\$nompc") -eq $true) 
           { 
+          
             Write-Host "repertoire existe"
             Write-Output "\\SRVACME\sav$\$nompc"
 
@@ -41,7 +48,7 @@ foreach ($repertoires in $ListeRepertoire)
         
         #lancement de robocopy et sauvegarde de Documents
         # la deuxième ligne peut servir pour sauvegarder les Favoris, au choix selon les cas
-        Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\users\$nomuser\Documents \\SRVACME\sav$\$nompc\$nomuser\Documents /copy:DAT /E /TS /FP /V /LOG+:\\SRVACME\sav$\$nompc.log /TEE " 
+       Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\users\$nomuser\Documents\ \\SRVACME\sav$\$nompc\$nomuser\Documents /V /FP /TS /LOG+:\\SRVACME\sav$\$nompc.log /TEE " 
         #Start-Process -FilePath "c:\windows\system32\robocopy.exe" -ArgumentList "c:\users\$nomuser\Favoris \\SRVACME\sav$\$nompc /copy:DAT /E /TS /FP /V /LOG+:c:\$nompc.log /TEE " 
 
       }
